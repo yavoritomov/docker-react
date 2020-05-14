d047dbd09cd8c30651c5f6542cbef36f88ea3fa3
Yavor Tomov <yavoritomov@gmail.com>
2020-05-14 22:09:37 +0300

    inital commit

---
+++ build/precache-manifest.fdc4cbe7b506a000dcc90c3085131466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de654360c4d198ce90ead3dca2da81d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8baae857cefa9e3fb6f8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5b734056f93cf4f02d3",
+    "url": "/static/js/2.b1327c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1327c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8baae857cefa9e3fb6f8",
+    "url": "/static/js/main.6c755f35.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
